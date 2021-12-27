3866a425e6d88b3db673d92da0d3ef5c68c94a01
link5401 <sieunganinh880@gmail.com>
2021-12-27 23:00:57 +0700

    f

--- workspace/logic_design/logic_des/display.ino
+++ workspace/logic_design/logic_des/display.ino
@@ -1,6 +1,8 @@
 #include "display.h"
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include "iot.h"
+#include <BlynkSimpleStream.h>
 const int DHTPIN = 8;      
 const int DHTTYPE = DHT11; 
 DHT dht(DHTPIN, DHTTYPE);
@@ -30,4 +32,23 @@ void display_init(){
     lcd.backlight();
     Serial.begin(9600);
     dht.begin();
+}
+///////////////////////////////////////////////////////////////
+
+ char auth[] = "NEGIHQRiIs4o_LT2XCFuiAjMJ3vE6bp7";
+void iot_setup(){
+   Serial.begin(9600);
+   Blynk.begin(Serial, auth);  
+}
+BLYNK_WRITE(V1) //Button Widget is writing to pin V1
+{
+  int pinData = param.asInt(); 
+  if(pinData==1){
+    digitalWrite(13, HIGH);
+  }else{
+    digitalWrite(13, LOW);
+  }
+}
+void iot_run(){
+    Blynk.run();  
 }
